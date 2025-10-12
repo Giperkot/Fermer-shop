1b80f285b08252f0d064223ee554ac9279a0515a
KettyT <ekaterina@example.com>
2025-10-12 13:45:00 +0500

    Доработка слайдера под Swiper

--- src/utils/WareCard.js
+++ src/utils/WareCard.js
@@ -2,14 +2,68 @@ import React, { useState } from 'react';
 
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/css/bundle';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 
 
 function WareCard() {
     const [state, setState] = useState({
         cardMode: "btn-description"
     });
-    console.log(state);
+    // console.log(state);
+
+
+
+    const swiper1 = <Swiper
+        modules={[Navigation, Pagination]}
+        slidesPerView={1}
+    >
+        <SwiperSlide>
+            <div className="ware-cards--description">
+                <div className="card-text ">
+                    <p>
+                        Филе бедра без кожи и кости. Птица содержится
+                        в свободных птичниках, выращивается на натуральных
+                        зерновых кормах, что влияет положительно на вкус мяса.
+                        Филейная часть бедра обладает насыщенным вкусом
+                        и мясным ароматом.
+                    </p>
+                </div>
+                <span className="price-background-blue">400 руб. / 700 гр.</span>
+            </div>
+
+        </SwiperSlide>
+        <SwiperSlide>
+            <div className="ware-cards--params">
+                <p>
+                    <b>Масса:</b> 0,7 кг. (595-805 г.).
+                </p>
+                <p>
+                    <b>Срок годности:</b> 6 суток
+                </p>
+                <p>
+                    <b>Порода:</b> КОББ 500.
+                </p>
+                <p>
+                    <b>Срок годности:</b> 6 суток.
+                </p>
+                <p>
+                    <b>Место происхождения:</b> Тверская область
+                </p>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+            <div className="ware-cards--properties">
+                <p>
+                    <b>Энергетическая ценность:</b> 135 ккал./565 кДж.
+                </p>
+                <p>
+                    <b>Пищевая ценность:</b> белки - 13,8 г., жиры - 8,7 г., углеводы - 0 г.; на 100 г.
+                </p>
+            </div>
+        </SwiperSlide>
+    </Swiper>;
+
+    const swiperSmth = useSwiper();
 
     return (
         <div className="ware-cards--wrap">
@@ -20,21 +74,28 @@ function WareCard() {
                     <div onClick={function (evt) {
                         // debugger;
                         if (evt.target.classList.contains("btn-description")) {
+                            swiper1.slideTo(1);
                             setState({
                                 cardMode: "btn-description"
                             });
+
                         }
 
                         if (evt.target.classList.contains("btn-params")) {
+                            // debugger;
+                            console.log(swiperSmth);
+                            swiperSmth.slideTo(2);
                             setState({
                                 cardMode: "btn-params"
                             });
+
                         }
 
                         if (evt.target.classList.contains("btn-properties")) {
                             setState({
                                 cardMode: "btn-properties"
                             });
+                            swiperSmth.slideTo(3);
                         }
                     }} >
                         <button className={state.cardMode === "btn-description" ?
@@ -44,46 +105,11 @@ function WareCard() {
                         <button className={state.cardMode === "btn-properties" ?
                             "btn btn-default btn-properties btn-active" : "btn btn-default btn-properties"}>Свойства</button>
                     </div>
-                    <div className={state.cardMode === "btn-description" ?
-                        "ware-cards--description" : "hidden"}>
-                        <div className="card-text ">
-                            <p>
-                                Филе бедра без кожи и кости. Птица содержится
-                                в свободных птичниках, выращивается на натуральных
-                                зерновых кормах, что влияет положительно на вкус мяса.
-                                Филейная часть бедра обладает насыщенным вкусом
-                                и мясным ароматом.
-                            </p>
-                        </div>
-                        <span className="price-background-blue">400 руб. / 700 гр.</span>
-                    </div>
-                    <div className={state.cardMode === "btn-params" ?
-                        "ware-cards--params" : "hidden"}>
-                        <p>
-                            <b>Масса:</b> 0,7 кг. (595-805 г.).
-                        </p>
-                        <p>
-                            <b>Срок годности:</b> 6 суток
-                        </p>
-                        <p>
-                            <b>Порода:</b> КОББ 500.
-                        </p>
-                        <p>
-                            <b>Срок годности:</b> 6 суток.
-                        </p>
-                        <p>
-                            <b>Место происхождения:</b> Тверская область
-                        </p>
-                    </div>
-                    <div className={state.cardMode === "btn-properties" ?
-                        "ware-cards--properties" : "hidden"}>
-                        <p>
-                            <b>Энергетическая ценность:</b> 135 ккал./565 кДж.
-                        </p>
-                        <p>
-                            <b>Пищевая ценность:</b> белки - 13,8 г., жиры - 8,7 г., углеводы - 0 г.; на 100 г.
-                        </p>
-                    </div>
+                    {swiper1}
+
+
+
+
                 </div>
             </div>
         </div>
